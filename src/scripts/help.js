06dfcbc4eabf995f176e309d809f21ad49782c6d
393667111@qq.com <393667111@qq.com>
2025-04-04 22:38:17 +0800

    docs(help.js): 更新使用说明以包含更多命令和示例
    更新了帮助文件中的使用说明，新增了交易、绘图、程序、Docker和开发相关的命令及其示例，以便用户更清晰地了解如何使用各个功能。

--- src/scripts/help.js
+++ src/scripts/help.js
@@ -83,9 +83,40 @@ commands.forEach(cmd => {
 });
 
 console.log(`
-持仓管理:
-  npm run list              显示当前持仓列表
-  npm run list:clear        清理已平仓数据
-  npm run list:delete ID    删除指定交易ID的所有相关记录
-  npm run monit            实时监控持仓状态
+使用说明:
+
+  交易相关:
+    open <空头资产> <多头资产> <金额>  开仓命令，支持简写币种名称
+    close <交易ID>                    平仓命令
+    list [clear]                     查看当前持仓列表，可选参数clear用于清理已平仓记录
+    list:clear                       清理已平仓数据
+    list:delete <tradeId>           删除指定交易ID的所有相关记录
+    monit                           实时监控持仓情况
+    grid                           查看网格交易盈亏统计
+    grid monit                     实时监控网格交易盈亏
+    grid monit <币种>               监控指定币种的网格交易盈亏
+
+  绘图相关:
+    graph orders                     切换主图上历史订单记录的显示/隐藏
+    graph trans                      切换主图上开平仓信息的显示/隐藏
+
+  程序相关:
+    start                           启动主程序
+    trading                         启动手动交易程序
+
+  Docker相关:
+    docker                          重新构建并运行Docker容器
+    docker:build                    构建Docker镜像
+    docker:run                      运行Docker容器
+    docker:logs                     查看Docker容器日志
+
+  开发相关:
+    lint                            检查代码规范
+    format                          格式化代码
+
+示例:
+  npm run open sol eth 2000         开仓2000USDT的SOL-ETH对冲头寸
+  npm run close 318fe6d8            平仓ID为318fe6d8的头寸
+  npm run list clear                清理并显示持仓列表
+  npm run grid monit BTC            监控BTC的网格交易盈亏
 `);
