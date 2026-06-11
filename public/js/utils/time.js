38a2d12537d42a577ea9026d66ab15415aab1829
393667111@qq.com <393667111@qq.com>
2026-06-11 13:05:19 +0800

    1

--- public/js/utils/time.js
+++ public/js/utils/time.js
@@ -2,7 +2,7 @@ window.TradingApp = window.TradingApp || {};
 window.TradingApp.Time = {
   updateTime: function () {
     const now = new Date();
-    const timeStr = now.toLocaleTimeString('zh-CN', { hour12: false });
+    const timeStr = now.toLocaleString('zh-CN', { hour12: false });
     document.getElementById('currentTime').textContent = timeStr;
   },
   startTimeUpdater: function () {
